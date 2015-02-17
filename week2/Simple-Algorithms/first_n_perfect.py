9b93f4d80e1175bebba9829dba434fb841d5c0c1
krisio <kristian.teod@gmail.com>
2015-02-17 16:36:53 +0200

    added perfects

--- week2/Simple-Algorithms/first_n_perfect.py
+++ week2/Simple-Algorithms/first_n_perfect.py
@@ -1,1 +1,21 @@
-#Working on
+n = input("Enter n: ")
+n = int(n)
+first = 6
+
+while True:
+    div_sum = 0
+    div = 1
+    is_perfect = False
+
+    while div < first:
+        if first % div == 0:
+            div_sum += div
+        div += 1
+    if div_sum == first:
+        is_perfect = True
+    if is_perfect:
+        print(first)
+        n = n - 1
+    if n == 0:
+        break
+    first += 1
